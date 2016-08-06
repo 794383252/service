e4c27e295277db14fad47d083387393c861cfbbc
lining <1507424744@qq.com>
2016-08-06 10:04:01 +0800

    server的使用二

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,68 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    server.myBind bind;
+    Button bind_server;
+    Button unbind_server;
+
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.i("ln", "这是onServiceConnected");
+            bind= (server.myBind) service;
+            bind.getProgress();
+            bind.startDownload();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("ln", "这是onServiceDisconnected");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i=new Intent(this,server.class);
-        startService(i);
+        init();
+
+
+    }
+
+    public void init() {
+        bind_server = (Button) findViewById(R.id.bind_server);
+        unbind_server = (Button) findViewById(R.id.unbind_server);
+
+        bind_server.setOnClickListener(this);
+        unbind_server.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bind_server:
+                Log.i("ln","执行点击事件");
+                //调用onServiceConnected
+                Intent bindintent = new Intent(MainActivity.this, server.class);
+                bindService(bindintent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_server:
+                unbindService(connection);
+                break;
+        }
     }
 }
--- src/main/java/com/example/myapplication/server.java
+++ src/main/java/com/example/myapplication/server.java
@@ -4,22 +4,25 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.os.SystemClock;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 /**
  * Created by Administrator on 2016/8/5.
- *
+ * <p/>
  * 由于service默认执行在主线程中，所以service中需要单开一个线程，不然很容易造成ANR，
  * 可以使用intentservice，集成了线程和自动关闭
  */
-public class server extends Service{
+public class server extends Service {
+
+    myBind myBind = new myBind();
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+
+        return myBind;
     }
 
     @Override
@@ -28,17 +31,39 @@ public class server extends Service{
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.i("ln","函数执行");
+                Log.i("ln", "函数执行");
             }
         }).start();
 
-        AlarmManager manager= (AlarmManager) getSystemService(ALARM_SERVICE);
-        int anHour=30*100;
-        long time= SystemClock.elapsedRealtime()+anHour;
-        Intent i = new Intent(this,boastcastrecevice.class);
-        PendingIntent pi=PendingIntent.getBroadcast(this,0,i,0);
-        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,time,pi);
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 30 * 100;
+        long time = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, boastcastrecevice.class);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, time, pi);
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    public class myBind extends Binder {
+        public void startDownload() {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("ln", "startDownload执行");
+                }
+            }).start();
+
+            AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            int anHour = 30 * 100;
+            long time = SystemClock.elapsedRealtime() + anHour;
+            Intent i = new Intent(server.this, boastcastrecevice.class);
+            PendingIntent pi = PendingIntent.getBroadcast(server.this, 0, i, 0);
+            manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, time, pi);
+        }
+
+        public void getProgress() {
+            Log.i("ln", "getProgress执行");
+        }
     }
 
 }
